a1ac6e9052182fc59863bb00cd01132b431661f3
IncreasonJr <delatorcaleb3@gmail.com>
2025-11-04 08:31:17 +0000

    Bank_Account-DAY 12.py

--- Bank_Account-DAY 12.py
+++ Bank_Account-DAY 12.py
@@ -1,48 +1,71 @@
 class Bank_Account:
+    def __init__(self):
+       self.balance = 0
+
     def owner_details(self):
-        global balance
         name =input('Enter your name: ').capitalize().strip()
         while not name:
           print("Name cannot be empty!")
           name =input('Enter your name: ').capitalize().strip()
-          continue
+        while not name.isalpha():
+            print('Name can only contain letters!Try again')
+            name =input('Enter your name: ').capitalize().strip()
         try:
-            balance = int(input('How much do you want to create the account with: '))
-            while balance <= 0:
+            self.balance = int(input('How much do you want to create the account with: '))
+            while self.balance <= 0:
                 print('Invalid amount, enter a valid greater than zero: ')
-                balance = int(input('How much do you want to create the account with: '))
+                self.balance = int(input('How much do you want to create the account with: '))
         except ValueError:
-            print('Invalid amount')
-        print(f'Account holder: {name} Initial_balance: GHC{balance}')
+          print('Invalid amount')
+        print(f'Account holder: {name} Initial_balance: GHC{self.balance}')
 
     def deposit(self):
-        global balance
-        deposit=int(input('How much do you want to deposit: '))
-        while deposit < 0:
-            print('Invalid amount.Enter a positive amount:  ')
+        try:
             deposit=int(input('How much do you want to deposit: '))
-        balance = balance + deposit
-        print(f'You deposited GHC{deposit:.2f}, your new balance is GHC{balance:.2f}')
+            while deposit < 0:
+             print('Invalid amount.Enter a positive amount:  ')
+             deposit=int(input('How much do you want to deposit: '))
+            self.balance = self.balance + deposit
+            print(f'You deposited GHC{deposit:.2f}, your new balance is GHC{self.balance:.2f}')
+        except ValueError:
+          print('Invalid amount')
 
     def withdraw(self):
-        global balance
         try:
             withdrawal_amount=int(input('How much do you want to withdraw: '))     
             while withdrawal_amount < 0:
                 print('Invalid amount.Enter a positive value: ')
                 withdrawal_amount=int(input('How much do you want to withdraw: '))  
-            while withdrawal_amount > balance:
-                print(f'Insufficient balance. You can only withdraw up to GHC{balance:.2f}')
+            while withdrawal_amount > self.balance:
+                print(f'Insufficient balance. You can only withdraw up to GHC{self.balance:.2f}')
                 withdrawal_amount=int(input('How much do you want to withdraw: '))  
-            balance = balance - withdrawal_amount
-            print(f'GHC{withdrawal_amount:.2f} has been withdrawn from your account. Your new balance is GHC{balance:.2f}')
+            self.balance = self.balance - withdrawal_amount
+            print(f'GHC{withdrawal_amount:.2f} has been withdrawn from your account. Your new balance is GHC{self.balance:.2f}')
         except ValueError: 
             print('Invalid amount')
         
 
+def main():
+    account1=Bank_Account()
+    account1.owner_details()
 
-account1=Bank_Account()
-account1.owner_details()
-account1.deposit()
 
-account1.withdraw()
+    while True:
+        print('This is your bank account')
+        print('1. Deposit ')
+        print('2. Withdraw')
+        print('3. Exit')
+        choice = input('What do you want to do today: ')
+        if choice == '1':
+            account1.deposit()
+        elif choice == '2':
+            account1.withdraw()
+        elif choice == '3':
+            print('Goodbye!')
+            break
+        else:
+            print('Invalid option')
+            break
+   
+main()
+
